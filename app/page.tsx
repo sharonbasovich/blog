cc54b4d9eac84d6c7f30ee4450ab7f0ab0080bd3
unknown <sharon@basovich.com>
2025-06-29 16:59:09 -0400

    setup async fetching from db with rsc

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import Image from "next/image";
-import Link from "next/link";
+import { prisma } from "./utils/db";
 
-export default function Home() {
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true,
+      createAt: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
-    <div>
-      <h1>Hello</h1>
-      <Link href="/dashboard">dashboards</Link>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <h1 key={item.title}>{item.title}</h1>
+        ))}
+      </div>
     </div>
   );
 }
