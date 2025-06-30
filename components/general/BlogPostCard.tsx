bf87a22002598ae2157e66ad9af4645248b67c15
unknown <sharon@basovich.com>
2025-06-29 23:45:55 -0400

    added blog cards

---
+++ components/general/BlogPostCard.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+// component for personal blog cards
+
+interface IappProps {
+  data: {
+    id: string;
+    title: string;
+    content: string;
+    imageUrl: string;
+    authorId: string;
+    authorName: string;
+    authorImage: string;
+    createAt: Date;
+    updatedAt: Date;
+  };
+}
+
+export function BlogPostCard({ data }: IappProps) {
+  return (
+    <div className="group relative overflow-hidden rounded-lg border border-gray-200 bg-white shadow-md transition-all hover:shadow-lg">
+      <Link className="block w-full h-full" href={`/post/${data.id}`}>
+        <div className="relative h-48 w-full overflow-hidden">
+          <Image
+            className="object-cover transition-transform duration-300 group-hover:scale-105"
+            src={data.imageUrl}
+            alt="Image for blog"
+            fill
+          ></Image>
+        </div>
+
+        <div className="p-4">
+          <h3 className="mb-2 text-lg font-semibold text-gray-900">
+            {data.title}
+          </h3>
+          <p className="mb-4 text-sm text-gray-600 line-clamp-2">
+            {data.content}
+          </p>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+              <div className="relative size-8 overflow-hidden rounded-full">
+                <Image src={data.authorImage} alt={data.authorName} fill className="object-cover"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      // this version is outdated
+      // {
+      //   hostname: "google.com",
+      //   protocol: "https",
+      //   port: "",
+      // },
+      new URL("https://placehold.co/600x400.png"),
+      new URL("https://lh3.googleusercontent.com"),
+    ],
+  },
 };
 
 export default nextConfig;
