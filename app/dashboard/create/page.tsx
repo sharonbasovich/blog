f9534323be09d99ab2d9b43746772f0fb0ace391
unknown <sharon@basovich.com>
2025-06-29 22:54:37 -0400

    mutations working

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -10,7 +10,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { prisma } from "@/app/utils/db";
+import { handleSubmission } from "@/app/actions";
 
 export default function CreateBlogRoute() {
   return (
@@ -23,18 +23,18 @@ export default function CreateBlogRoute() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" action={handleSubmission}>
             <div className="flex flex-col gap-2">
               <Label>Title</Label>
-              <Input required type="text" placeholder="Title"/>
+              <Input name="title" required type="text" placeholder="Title"/>
             </div>
             <div className="flex flex-col gap-2">
               <Label>Content</Label>
-              <Textarea required placeholder="Content"/>
+              <Textarea name="content" required placeholder="Content"/>
             </div>
             <div className="flex flex-col gap-2">
               <Label>Image URL</Label>
-              <Input required type="url" placeholder="Image URL"/>
+              <Input name="url" required type="url" placeholder="Image URL"/>
             </div>
             <Button>Create Post</Button>
           </form>
