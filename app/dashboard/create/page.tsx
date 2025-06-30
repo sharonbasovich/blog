420cfd064f6dea24470b9b5c861dd43eab0d216a
unknown <sharon@basovich.com>
2025-06-29 22:32:40 -0400

    setup create post form

---
+++ app/dashboard/create/page.tsx
@@ -0,0 +1,45 @@
+"use client"; // CLIENT SIDE RENDERING
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/app/utils/db";
+
+export default function CreateBlogRoute() {
+  return (
+    <div>
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Create Post</CardTitle>
+          <CardDescription>
+            Create a new post to share with the world
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <Label>Title</Label>
+              <Input required type="text" placeholder="Title"/>
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label>Content</Label>
+              <Textarea required placeholder="Content"/>
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label>Image URL</Label>
+              <Input required type="url" placeholder="Image URL"/>
+            </div>
+            <Button>Create Post</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,14 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function dashboardRoute() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
 
-  if (!user) {
-    return redirect("/api/auth/register");
-  }
-
-  return <div>test</div>;
+        <Link className={buttonVariants()} href="/dashboard/create">Create Post</Link>
+      </div>
+    </div>
+  );
 }
