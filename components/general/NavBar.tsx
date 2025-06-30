28fc50ab3db0be0ede92808b8ecaa99c3d22182d
unknown <sharon@basovich.com>
2025-06-30 01:44:27 -0400

    fixed caching

--- components/general/NavBar.tsx
+++ components/general/NavBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import {
@@ -5,11 +7,11 @@ import {
   LoginLink,
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
-export async function NavBar() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+export function NavBar() {
+  const { getUser } = useKindeBrowserClient();
+  const user = getUser();
   return (
     <nav className="py-5 flex items-center justify-between">
       <div className="flex items-center gap-6">
