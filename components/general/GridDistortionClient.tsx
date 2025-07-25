6b581edefec1f8ea2fb4577fa5e7fe774c8e7708
unknown <sharon@basovich.com>
2025-07-25 03:00:23 -0400

    fixed ts error

--- components/general/GridDistortionClient.tsx
+++ components/general/GridDistortionClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 import GridDistortion from "./GridDistortion";
+import type { GridDistortionProps } from "./GridDistortion";
 
-export default function GridDistortionClient(props) {
+export default function GridDistortionClient(props: GridDistortionProps) {
   return <GridDistortion {...props} />;
 }
